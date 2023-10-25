c0ad6bb9a1d67ed2243a3142294955e4f3682acb
SupriyaBikki <bikki.usedu@gmail.com>
2023-10-25 17:49:05 -0500

    new end point

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
